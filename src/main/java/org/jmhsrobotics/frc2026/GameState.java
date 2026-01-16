9e35cf7e1ff05cb663ee3760aeb0cfd8bf451420
Derek <dereksun31@gmail.com>
2026-01-15 20:31:54 -0500

    GameState updated

--- src/main/java/org/jmhsrobotics/frc2026/GameState.java
+++ src/main/java/org/jmhsrobotics/frc2026/GameState.java
@@ -3,39 +3,32 @@ package frc.robot;
 import edu.wpi.first.wpilibj.DriverStation;
 
 public class GameState {
-
-    public enum AllianceColor {
-        RED, BLUE, UNKNOWN
-    }
     public enum hubStatus {
         ACTIVE, INACTIVE, UNKNOWN
     }
 
-    private AllianceColor currentAlliance = AllianceColor.UNKNOWN;
+    // private AllianceColor currentAlliance = AllianceColor.UNKNOWN;
+
+    Optional<Alliance> ally = DriverStation.getAlliance();
+    //import ^^
 
     public void updateGameState() {
-        String gameData = DriverStation.getGameSpecificMessage();
-        
-        if (gameData != null && gameData.length() > 0) {
-            switch (gameData.charAt(0)) {
-                case 'B':
-                    currentAlliance = AllianceColor.BLUE;
-                    break;
-                case 'R':
-                    currentAlliance = AllianceColor.RED;
-                    break;
-                default:
-                    currentAlliance = AllianceColor.UNKNOWN;
-                    break;
+        String gameData;
+        gameData = DriverStation.getGameSpecificMessage();
+        if(gameData.length() > 0){
+            if(gameData.charAt(0) == "R" &&  ally.get() == Alliance.Red){
+                //finish logic
             }
-
-
-
-        } else {
-            currentAlliance = AllianceColor.UNKNOWN;
+            else if(gameData.charAt(0) == "B"){
+                //finish logic
+            }
+            else{
+                //else, if we dont know what hub is active
+            }
+        }
+        else{
+            //else if we dont receive any data
         }
     }
-    public AllianceColor getState(){
-        return currentAlliance;
-    }
+    //eventual return statement
 }
