88a16b4627efbf5db1b39e42db60358a2c088fc7
Colin <arcxel974@gmail.com>
2026-01-15 20:21:22 -0500

    removed example files

--- src/main/java/org/jmhsrobotics/frc2026/RobotContainer.java
+++ src/main/java/org/jmhsrobotics/frc2026/RobotContainer.java
@@ -8,9 +8,6 @@ import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
 import edu.wpi.first.wpilibj2.command.button.Trigger;
 import org.jmhsrobotics.frc2026.Constants.OperatorConstants;
-import org.jmhsrobotics.frc2026.commands.Autos;
-import org.jmhsrobotics.frc2026.commands.ExampleCommand;
-import org.jmhsrobotics.frc2026.subsystems.ExampleSubsystem;
 
 /**
  * This class is where the bulk of the robot should be declared. Since Command-based is a
@@ -19,8 +16,6 @@ import org.jmhsrobotics.frc2026.subsystems.ExampleSubsystem;
  * subsystems, commands, and trigger mappings) should be declared here.
  */
 public class RobotContainer {
-  // The robot's subsystems and commands are defined here...
-  private final ExampleSubsystem m_exampleSubsystem = new ExampleSubsystem();
 
   // Replace with CommandPS4Controller or CommandJoystick if needed
   private final CommandXboxController m_driverController =
@@ -42,9 +37,6 @@ public class RobotContainer {
    * joysticks}.
    */
   private void configureBindings() {
-    // Schedule `ExampleCommand` when `exampleCondition` changes to `true`
-    new Trigger(m_exampleSubsystem::exampleCondition)
-        .onTrue(new ExampleCommand(m_exampleSubsystem));
 
     // Schedule `exampleMethodCommand` when the Xbox controller's B button is pressed,
     // cancelling on release.
@@ -57,6 +49,6 @@ public class RobotContainer {
    */
   public Command getAutonomousCommand() {
     // An example command will be run in autonomous
-    return Autos.exampleAuto(m_exampleSubsystem);
+    return null;
   }
 }
