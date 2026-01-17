d25392e120cbe3b8c8daf10fdb92631c7f4bf0c6
Derek <dereksun31@gmail.com>
2026-01-16 20:17:34 -0500

    Working GameState Class, work on getHubStatus method

--- src/main/java/org/jmhsrobotics/frc2026/GameState.java
+++ src/main/java/org/jmhsrobotics/frc2026/GameState.java
@@ -1,34 +1,64 @@
+package org.jmhsrobotics.frc2026;
 
-package frc.robot;
 import edu.wpi.first.wpilibj.DriverStation;
+import edu.wpi.first.wpilibj.DriverStation.Alliance;
+
+import java.util.Optional;
 
 public class GameState {
-    public enum hubStatus {
-        ACTIVE, INACTIVE, UNKNOWN
+  public static final int TELEOP_SEC = 140;
+  public static final int ENDGAME_SEC = 30;
+  public static final int AUTO_SEC = 20;
+  public static final int TRANS_LENGTH_SEC = 10;
+  public enum Hub{
+    ACTIVE,
+    INACTIVE,
+    UNKNOWN
+  }
+  public static Hub getHubStatus() {
+    double time = DriverStation.getMatchTime();
+    boolean isAuto = DriverStation.isAutonomous();
+    Alliance alliance = getAlliance(DriverStation.getAlliance());
+    // Optional<Alliance> ally =
+    boolean wonAuto = getWonAuto(DriverStation.getGameSpecificMessage(), alliance);
+    //determine which quarter we are in
+    int quarter = (TELEOP_SEC-TRANS_LENGTH_SEC-ENDGAME_SEC) / 25;
+    
+
+    if(isAuto){ // auto
+        return Hub.ACTIVE;
     }
-
-    // private AllianceColor currentAlliance = AllianceColor.UNKNOWN;
-
-    Optional<Alliance> ally = DriverStation.getAlliance();
-    //import ^^
-
-    public void updateGameState() {
-        String gameData;
-        gameData = DriverStation.getGameSpecificMessage();
-        if(gameData.length() > 0){
-            if(gameData.charAt(0) == "R" &&  ally.get() == Alliance.Red){
-                //finish logic
-            }
-            else if(gameData.charAt(0) == "B"){
-                //finish logic
-            }
-            else{
-                //else, if we dont know what hub is active
-            }
-        }
-        else{
-            //else if we dont receive any data
-        }
+    if(time <= ENDGAME_SEC){ // endgame
+        return Hub.ACTIVE;
     }
-    //eventual return statement
+    if(time >= TELEOP_SEC - TRANS_LENGTH_SEC){ // transition
+        return Hub.ACTIVE;
+    }
+    if(quarter % 2 == (wonAuto?0:1) ){ // first and third quarter and we won, or second and fourth quarter and we lost
+        return Hub.INACTIVE;
+    }
+    else{ // first and third quarter and we lost, or second and fourth quarter and we won
+        return Hub.ACTIVE;
+    }
+    // return Hub.UNKNOWN;
+  }
+
+  private static Alliance getAlliance(Optional<Alliance> ally){
+    if(!ally.isPresent()){
+        return Alliance.Red;
+    }
+    return ally.get();
+  }
+  private static boolean getWonAuto(String gameData, Alliance ally){
+    if(gameData.length() < 1 || gameData == null){
+        return false;
+
+    }
+    if((gameData.charAt(0) == 'R' && (ally == Alliance.Red)) || (gameData.charAt(0) == 'B' && (ally == Alliance.Blue))){
+        return true; // we won auto
+    }
+    else{
+        return false; // we didn't win auto
+    }
+  }
 }
