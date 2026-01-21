08beeef7da14b8bfd2955e0ece7546d92742f1b6
Derek <dereksun31@gmail.com>
2026-01-20 20:30:07 -0500

    "Updated the lirbary references to the deprecated enum"

--- src/main/java/org/jmhsrobotics/frc2026/subsystems/drive/swerve/ModuleIORev.java
+++ src/main/java/org/jmhsrobotics/frc2026/subsystems/drive/swerve/ModuleIORev.java
@@ -15,12 +15,12 @@ package org.jmhsrobotics.frc2026.subsystems.drive.swerve;
 
 import com.revrobotics.AbsoluteEncoder;
 import com.revrobotics.RelativeEncoder;
+import com.revrobotics.ResetMode;
+import com.revrobotics.PersistMode;
 import com.revrobotics.spark.ClosedLoopSlot;
 import com.revrobotics.spark.FeedbackSensor;
 import com.revrobotics.spark.SparkBase;
 import com.revrobotics.spark.SparkBase.ControlType;
-import com.revrobotics.spark.SparkBase.PersistMode;
-import com.revrobotics.spark.SparkBase.ResetMode;
 import com.revrobotics.spark.SparkClosedLoopController;
 import com.revrobotics.spark.SparkClosedLoopController.ArbFFUnits;
 import com.revrobotics.spark.SparkLowLevel.MotorType;
