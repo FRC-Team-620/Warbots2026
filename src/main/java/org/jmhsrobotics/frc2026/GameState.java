78e7f0d5b79ba0c69bf7e6e3ed8d652c1694db8d
Derek <dereksun31@gmail.com>
2026-01-22 19:51:04 -0500

    Implement Requested Changes
    I implemented the requested changes, so now there is comments on the code, and removed the dashboard logging.

--- src/main/java/org/jmhsrobotics/frc2026/GameState.java
+++ src/main/java/org/jmhsrobotics/frc2026/GameState.java
@@ -2,7 +2,6 @@ package org.jmhsrobotics.frc2026;
 
 import edu.wpi.first.wpilibj.DriverStation;
 import edu.wpi.first.wpilibj.DriverStation.Alliance;
-import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import java.util.Optional;
 
 public class GameState {
@@ -20,20 +19,6 @@ public class GameState {
   public static Hub getHubStatus() {
     double time = DriverStation.getMatchTime();
     boolean isAuto = DriverStation.isAutonomous();
-    Alliance alliance = getAlliance(DriverStation.getAlliance());
-    boolean wonAuto = getWonAuto(DriverStation.getGameSpecificMessage(), alliance);
-
-    // determine which quarter we are in
-    int quarter = (int) ((time - ENDGAME_SEC) / 25.0);
-
-    // SmartDashboard.putNumber("Actual Switch Time", TELEOP_SEC-TRANS_LENGTH_SEC-ENDGAME_SEC);
-    SmartDashboard.putNumber("Switch Time", time - ENDGAME_SEC);
-    SmartDashboard.putNumber("Quarter", quarter);
-    SmartDashboard.putBoolean("TransitionReturnsActive", time >= TELEOP_SEC - TRANS_LENGTH_SEC);
-    SmartDashboard.putBoolean("FirstShiftActivity", quarter % 2 == (wonAuto ? 1 : 0));
-    SmartDashboard.putBoolean("WonAuto", wonAuto);
-    SmartDashboard.putNumber("Time ", DriverStation.getMatchTime());
-    SmartDashboard.putBoolean("In Auto ", DriverStation.isAutonomous());
 
     if (isAuto) { // auto
       return Hub.ACTIVE;
@@ -44,6 +29,12 @@ public class GameState {
     if (time >= TELEOP_SEC - TRANS_LENGTH_SEC) { // transition
       return Hub.ACTIVE;
     }
+
+    // determine which quarter we are in
+    int quarter = (int) ((time - ENDGAME_SEC) / 25.0);
+    Alliance alliance = getAlliance(DriverStation.getAlliance());
+    boolean wonAuto = getWonAuto(DriverStation.getGameSpecificMessage(), alliance);
+
     if (quarter % 2
         == (wonAuto
             ? 1
@@ -56,6 +47,7 @@ public class GameState {
 
   private static Alliance getAlliance(Optional<Alliance> ally) {
     if (!ally.isPresent()) {
+      // add some sort of alert/behavior change if we can not get are Alliance Color
       return Alliance.Red;
     }
     return ally.get();
@@ -67,6 +59,7 @@ public class GameState {
     }
     if ((gameData.charAt(0) == 'R' && (ally == Alliance.Red))
         || (gameData.charAt(0) == 'B' && (ally == Alliance.Blue))) {
+      // add some sort of alert/behavior change if we can not able to get Game data.
       return true; // we won auto
     } else {
       return false; // we didn't win auto
