07e6e3f0c7d5c6d49760ed0a2a1ae892aab5fffc
Tater448 <181247200+Tater448@users.noreply.github.com>
2026-02-03 19:41:31 -0500

    Started intake

--- src/main/java/org/jmhsrobotics/frc2026/subsystems/intake/IntakeIO.java
+++ src/main/java/org/jmhsrobotics/frc2026/subsystems/intake/IntakeIO.java
@@ -1,13 +1,14 @@
 package org.jmhsrobotics.frc2026.subsystems.intake;
 
 import org.littletonrobotics.junction.AutoLog;
-import org.littletonrobotics.junction.Logger;
 
 public interface IntakeIO {
-    @AutoLog
-    public static class IntakeIOInputs {
-        
-    }
+  @AutoLog
+  public static class IntakeIOInputs {}
 
-    public default void updateInputs(IntakeIOInputs inputs) {}
+  public default void updateInputs(IntakeIOInputs inputs) {}
+
+  public default void set(double speedDutyCycle) {}
+
+  public default void setBrakeMode(boolean enable) {}
 }
