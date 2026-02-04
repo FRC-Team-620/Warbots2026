f12b64621ee8716c0b31847abffe4e6f292f1827
Arcxel <arcxel974@gmail.com>
2026-02-03 19:20:41 -0500

    created intakeio interface

--- src/main/java/org/jmhsrobotics/frc2026/subsystems/intake/IntakeIO.java
+++ src/main/java/org/jmhsrobotics/frc2026/subsystems/intake/IntakeIO.java
@@ -1,13 +1,24 @@
 package org.jmhsrobotics.frc2026.subsystems.intake;
 
 import org.littletonrobotics.junction.AutoLog;
-import org.littletonrobotics.junction.Logger;
 
 public interface IntakeIO {
     @AutoLog
     public static class IntakeIOInputs {
-        
+        public double currentAMPS;
+        public double positionDEGREES;
+        public double tempC;
+        public double velocityRPM;
+        public double voltage;
     }
+
+    public default void setPIDF(double p, double i, double d, double f) {}
+
+    public default void setPosition(double degrees) {}
+
+    public default void setRPM(double rpm) {}
+    
+    public default void stop() {}
 
     public default void updateInputs(IntakeIOInputs inputs) {}
 }
