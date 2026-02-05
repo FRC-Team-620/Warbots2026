9263a6e5094ab3a96e4f4fc91a5381d191a19e69
Arcxel <arcxel974@gmail.com>
2026-02-05 18:52:10 -0500

    Created DoubleControl.java

--- src/main/java/org/jmhsrobotics/frc2026/controlBoard/DoubleControl.java
+++ src/main/java/org/jmhsrobotics/frc2026/controlBoard/DoubleControl.java
@@ -5,31 +5,31 @@ import java.util.function.DoubleSupplier;
 import org.jmhsrobotics.frc2026.util.ControllerMonitor;
 
 public class DoubleControl implements ControlBoard {
-    CommandXboxController driver = new CommandXboxController(0);
+  CommandXboxController driver = new CommandXboxController(0);
 
-    public DoubleControl() {
-        ControllerMonitor.addController(driver.getHID(), "Driver");
-    }
+  public DoubleControl() {
+    ControllerMonitor.addController(driver.getHID(), "Driver");
+  }
 
-    // ========Driver Controls========
+  // ========Driver Controls========
 
-    @Override
-    public double rotation() {
-        return driver.getRightX();
-    }
+  @Override
+  public double rotation() {
+    return driver.getRightX();
+  }
 
-    @Override
-    public double translationX() {
-        return driver.getLeftX();
-    }
+  @Override
+  public double translationX() {
+    return driver.getLeftX();
+  }
 
-    @Override
-    public double translationY() {
-        return driver.getLeftY();
-    }
+  @Override
+  public double translationY() {
+    return driver.getLeftY();
+  }
 
-    @Override
-    public DoubleSupplier shoot() {
-        return () -> driver.getRightTriggerAxis();
-    }
+  @Override
+  public DoubleSupplier shoot() {
+    return () -> driver.getRightTriggerAxis();
+  }
 }
