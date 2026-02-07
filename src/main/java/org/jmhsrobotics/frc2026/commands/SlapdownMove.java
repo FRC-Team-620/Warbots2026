63dadde9f7ce57e2f4eac9185e2bcb30b6574323
Derek <dereksun31@gmail.com>
2026-02-06 19:11:49 -0500

    Make SlapdownMove.java
    Made the command to move the intake up/down, and assigned it to buttons for single & double controller.

---
+++ src/main/java/org/jmhsrobotics/frc2026/commands/SlapdownMove.java
@@ -0,0 +1,25 @@
+package org.jmhsrobotics.frc2026.commands;
+
+import edu.wpi.first.wpilibj2.command.Command;
+import org.jmhsrobotics.frc2026.subsystems.intake.Intake;
+
+public class SlapdownMove extends Command {
+  private Intake intake;
+  private double goalDegrees;
+
+  public SlapdownMove(Intake intake, double goalDegrees) {
+    this.intake = intake;
+    this.goalDegrees = goalDegrees;
+    addRequirements(this.intake);
+  }
+
+  @Override
+  public void initialize() {
+    this.intake.setSetpoint(goalDegrees);
+  }
+
+  @Override
+  public boolean isFinished() {
+    return this.intake.atGoal();
+  }
+}
