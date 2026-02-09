df041577d42d1dc64bb143e4762ee3a6630aa28f
m10653 <m10653@users.noreply.github.com>
2026-02-08 19:11:04 -0500

    Flx Flywheel sim

--- src/main/java/org/jmhsrobotics/frc2026/commands/ShooterMove.java
+++ src/main/java/org/jmhsrobotics/frc2026/commands/ShooterMove.java
@@ -18,16 +18,17 @@ public class ShooterMove extends Command {
 
   @Override
   public void initialize() {
-    this.shooter.set(0);
+    this.shooter.setRPM(0);
   }
 
   @Override
   public void execute() {
     double rightTrigger = rightTriggerAxis.getAsDouble();
-    if (rightTrigger > 0) this.shooter.set(Constants.ShooterConstants.kBaseRPM);
-
-    if (rightTrigger == 0) {
-      this.shooter.set(0.0);
+    if (rightTrigger > 0) {
+      this.shooter.setRPM(Constants.ShooterConstants.kBaseRPM);
+    } else {
+      // this.shooter.setRPM(0.0);
+      this.shooter.stop(); //Do not use Power to Spindown flywheels
     }
   }
 
@@ -38,6 +39,7 @@ public class ShooterMove extends Command {
 
   @Override
   public void end(boolean interrupted) {
-    this.shooter.set(0);
+    this.shooter.stop(); //Do not use Power to Spindown flywheels
+    // this.shooter.setRPM(0);
   }
 }
