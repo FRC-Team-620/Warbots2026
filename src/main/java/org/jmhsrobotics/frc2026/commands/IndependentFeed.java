e00760eacc4a2ad7dacb55ce80c6ac8cf7049f3c
Tater448 <181247200+Tater448@users.noreply.github.com>
2026-03-05 19:06:49 -0500

    Refactor Speed to Duty Cycle in IndependentFeed

--- src/main/java/org/jmhsrobotics/frc2026/commands/IndependentFeed.java
+++ src/main/java/org/jmhsrobotics/frc2026/commands/IndependentFeed.java
@@ -3,24 +3,26 @@ package org.jmhsrobotics.frc2026.commands;
 import edu.wpi.first.wpilibj2.command.Command;
 import org.jmhsrobotics.frc2026.subsystems.feeder.Feeder;
 
+/* This command is runs the feeder without needing the shooter to be at its goal rpm. It is meant to be used with duty cycle shoot */
+
 public class IndependentFeed extends Command {
   private Feeder feeder;
-  private double speed;
+  private double dutyCycle;
 
-  public IndependentFeed(Feeder feeder, double speed) {
+  public IndependentFeed(Feeder feeder, double dutyCycle) {
     this.feeder = feeder;
-    this.speed = speed;
+    this.dutyCycle = dutyCycle;
     addRequirements(this.feeder);
   }
 
   @Override
   public void initialize() {
-    this.feeder.setFeederSpeed(0);
+    this.feeder.setFeederSpeed(dutyCycle);
   }
 
   @Override
   public void execute() {
-    this.feeder.setFeederSpeed(speed);
+    this.feeder.setFeederSpeed(dutyCycle);
   }
 
   @Override
